a272606a387e45e33d4590f4138a5ebc7022293d
Coursera_Capstone <59306212+Jtonight@users.noreply.github.com>
2019-12-28 20:33:58 +1100

    Add greetings

---
+++ Coursera_Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Coursera_Capstone
+# 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
